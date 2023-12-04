f140df95ad9c5e2fa6428d14afd4529b54d81ee0
LakshanaSaminathan <110972475+LakshanaSaminathan@users.noreply.github.com>
2023-12-05 03:40:50 +0530

    Update admin_login.php

--- railway-management-system-master/admin_login.php
+++ railway-management-system-master/admin_login.php
@@ -14,14 +14,14 @@ $_SESSION["admin_login"]=True;
 
 if($_SESSION["admin_login"]==True)
 {
-echo " <br><a href=\"http://localhost/railway/insert_into_stations.php\"> Show All Stations </a><br> ";
-echo " <br><a href=\"http://localhost/railway/show_trains.php\"> Show All Trains </a><br> ";
-echo " <br><a href=\"http://localhost/railway/show_users.php\"> Show All Users </a><br> ";
-echo " <br><a href=\"http://localhost/railway/insert_into_train_3.php\"> Enter New Train </a><br> ";
-echo " <br><a href=\"http://localhost/railway/insert_into_classseats_3.php\"> Enter Train Schedule </a><br> ";
-echo " <br><a href=\"http://localhost/railway/booked.php\"> View all booked tickets </a><br> ";
-echo " <br><a href=\"http://localhost/railway/cancelled.php\"> View all cancelled tickets </a><br> ";
-//echo " <br><a href=\"http://localhost/railway/logout.php\"> Logout </a><br> ";
+echo " <br><a href=\"insert_into_stations.php\"> Show All Stations </a><br> ";
+echo " <br><a href=\"show_trains.php\"> Show All Trains </a><br> ";
+echo " <br><a href=\"show_users.php\"> Show All Users </a><br> ";
+echo " <br><a href=\"insert_into_train_3.php\"> Enter New Train </a><br> ";
+echo " <br><a href=\"insert_into_classseats_3.php\"> Enter Train Schedule </a><br> ";
+echo " <br><a href=\"booked.php\"> View all booked tickets </a><br> ";
+echo " <br><a href=\"cancelled.php\"> View all cancelled tickets </a><br> ";
+//echo " <br><a href=\"logout.php\"> Logout </a><br> ";
 }
 else 
 {
