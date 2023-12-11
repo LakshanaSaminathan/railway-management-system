8709c64f1dd5aeb715ead61c34cd11efc6436a92
LakshanaSaminathan <110972475+LakshanaSaminathan@users.noreply.github.com>
2023-12-11 09:40:35 +0530

    Update show_users.php

--- railway-management-system-master/show_users.php
+++ railway-management-system-master/show_users.php
@@ -19,7 +19,7 @@ echo "
 while ($cdrow=mysqli_fetch_array($cdresult)) 
 {
 	echo "
-<tr><td>".$cdrow[0]."</td><td>".$cdrow[1]."</td><td>".$cdrow[2]."</td><td>".$cdrow[3]."</td><td>".$cdrow[4]."</td><td><a href=\"http://localhost/railway/edit_user.php?id=".$cdrow[0]."\"><button>Edit</button></a></td><td><a href=\"http://localhost/railway/delete_user.php?id=".$cdrow[0]."\"><button>Delete</button></a></td></tr>
+<tr><td>".$cdrow[0]."</td><td>".$cdrow[1]."</td><td>".$cdrow[2]."</td><td>".$cdrow[3]."</td><td>".$cdrow[4]."</td><td><a href=\"edit_user.php?id=".$cdrow[0]."\"><button>Edit</button></a></td><td><a href=\"http://localhost/railway/delete_user.php?id=".$cdrow[0]."\"><button>Delete</button></a></td></tr>
 ";
 }
 echo "</table>";
